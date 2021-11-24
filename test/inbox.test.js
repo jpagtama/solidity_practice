5c8707ba18983a9c85a234a106c2dad3bc430ea9
Julian Pagtama <jpagtama@gmail.com>
2021-11-24 10:24:00 -0800

    Adds deployment with Web3 test

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -7,19 +7,29 @@ const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 //in order for web3 to communicate with a network it needs a provider
 //the provider is like a phone that communicates between web3 and ganache
+const { interface, bytecode } = require('../compile');
 
 let accounts;
+let inbox;
 
 beforeEach(async () => {
     //get a list of all accounts
     accounts = await web3.eth.getAccounts();
 
     //use one of those accounts to deploy the contract
-
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({
+            data: bytecode,
+            arguments: ['Hi there!']
+        })
+        .send({ 
+            from: accounts[0],
+            gas: '1000000'
+         });
 });
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
-        console.log("accounts",accounts);
+        console.log("inbox", inbox );
     });
 });
